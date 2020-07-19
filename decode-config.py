45085e283a5776dca70ca576eb8784f118143fc2
Norbert Richter <norbert.richter@prsolution.eu>
2020-07-19 14:36:39 +0200

    Update version

--- decode-config.py
+++ decode-config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-VER = '8.3.1.6 [00173]'
+VER = '8.3.1.7 [00173]'
 
 """
     decode-config.py - Backup/Restore Tasmota configuration data
