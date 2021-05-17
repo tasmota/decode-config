9df8beddfc5e473e7ce1dc1c5efa490f74316b7d
Norbert Richter <norbert.richter@prsolution.eu>
2021-05-17 12:06:04 +0200

    Fix prg version

--- decode-config.py
+++ decode-config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-VER = '9.4.0.0'
+VER = '9.4.0.3'
 
 """
     decode-config.py - Backup/Restore Tasmota configuration data
