30ab8f98843829498550d13895ae8882e70340ce
Norbert Richter <norbert.richter@prsolution.eu>
2022-04-20 11:26:30 +0200

    Update v11.1.0.1 settings

--- decode-config.py
+++ decode-config.py
@@ -2498,6 +2498,9 @@ SETTING_11_1_0_1 = copy.deepcopy(SETTING_11_0_0_7)
 SETTING_11_1_0_1.update            ({
     'energy_kWhexport_ph':          (HARDWARE.ESP,   '<l',  0xF7C,       ([3], '0 <= $ <= 4294967295',          ('Power',       '"EnergyExportActive{} {}".format(#+1,int(round(float($)//100)))')) ),
                                     })
+SETTING_11_1_0_1['flag5'][1].update({
+        'mm_vs_inch':               (HARDWARE.ESP,   '<L', (0xFB4,1,25), (None, None,                           ('SetOption',   '"SO139 {}".format($)')) ),
+                                    })
 # ======================================================================
 SETTINGS = [
             (0x0B010001,0x1000, SETTING_11_1_0_1),
