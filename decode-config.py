39d0a773235d11f5bb6ee1ce963a778b9447177d
Norbert Richter <norbert.richter@prsolution.eu>
2022-06-27 15:23:34 +0200

    Update v12.0.1.2 settings

--- decode-config.py
+++ decode-config.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 METADATA = {
-    'VERSION': '12.0.0.1',
+    'VERSION': '12.0.1.2',
     'DESCRIPTION': 'Backup/restore and decode configuration tool for Tasmota',
     'CLASSIFIER': 'Development Status :: 4 - Beta',
     'URL': 'https://github.com/tasmota/decode-config',
@@ -2609,13 +2609,13 @@ SETTING_11_1_0_4['flag5'][1].update({
         'wait_for_wifi_result':     (HARDWARE.ESP,   '<L', (0xFB4,1,28), (None, None,                           ('SetOption',   '"SO142 {}".format($)')) ),
                                     })
 # ======================================================================
-SETTING_12_0_0_1 = copy.deepcopy(SETTING_11_1_0_4)
-SETTING_12_0_0_1.update              ({
+SETTING_12_0_1_2 = copy.deepcopy(SETTING_11_1_0_4)
+SETTING_12_0_1_2.update              ({
     'dns_timeout':                  (HARDWARE.ESP,   '<H',  0x4C8,       (None, '100 <= $ <= 20000',            ('Wifi',        '"DnsTimeout {}".format($)')) ),
                                     })
 # ======================================================================
 SETTINGS = [
-            (0x0C000001,0x1000, SETTING_12_0_0_1),
+            (0x0C000102,0x1000, SETTING_12_0_1_2),
             (0x0B010004,0x1000, SETTING_11_1_0_4),
             (0x0B010003,0x1000, SETTING_11_1_0_3),
             (0x0B010002,0x1000, SETTING_11_1_0_2),
