1777c89e25f99d1517201e1e4e84067287a7c73f
Norbert Richter <norbert.richter@prsolution.eu>
2022-12-21 16:27:52 +0100

    Bump to v12.3.1.2

--- decode-config.py
+++ decode-config.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 METADATA = {
-    'VERSION': '12.3.1.1',
+    'VERSION': '12.3.1.2',
     'DESCRIPTION': 'Backup/restore and decode configuration tool for Tasmota',
     'CLASSIFIER': 'Development Status :: 5 - Production/Stable',
     'URL': 'https://github.com/tasmota/decode-config',
@@ -2700,8 +2700,11 @@ SETTING_12_2_0_6.update             ({
 # ======================================================================
 SETTING_12_3_1_1 = copy.deepcopy(SETTING_12_2_0_6)
 # ======================================================================
+SETTING_12_3_1_2 = copy.deepcopy(SETTING_12_3_1_1)
+# ======================================================================
 SETTINGS = [
-            (0x0C030100,0x1000, SETTING_12_3_1_1),
+            (0x0C030102,0x1000, SETTING_12_3_1_2),
+            (0x0C030101,0x1000, SETTING_12_3_1_1),
             (0x0C020006,0x1000, SETTING_12_2_0_6),
             (0x0C020005,0x1000, SETTING_12_2_0_5),
             (0x0C020004,0x1000, SETTING_12_2_0_4),
