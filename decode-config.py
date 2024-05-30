c41c253aff33e14fc9df84699a77e46d0f68277b
Norbert Richter <norbert.richter@prsolution.eu>
2024-05-30 10:02:56 +0200

    Update v14.0.0.2 settings

--- decode-config.py
+++ decode-config.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 METADATA = {
-    'VERSION': '14.0.0.1',
+    'VERSION': '14.0.0.2',
     'DESCRIPTION': 'Backup/restore and decode configuration tool for Tasmota',
     'CLASSIFIER': 'Development Status :: 4 - Beta',
     'URL': 'https://github.com/tasmota/decode-config',
@@ -2837,10 +2837,13 @@ SETTING_13_4_0_4.update             ({
      'power_lock':                  (HARDWARE.ESP,   '<L',  0xF9C,       (None, None,                           ('Power',       '"PowerLock0 0" if 0==int($) else "PowerLock0 1" if 0xffffffff==int($) else list("PowerLock{} {}".format(i+1, (int($)>>i & 1) ) for i in range(0, 32))')) ),
                                     })
 # ======================================================================
-SETTING_14_0_0_1 = copy.copy(SETTING_13_4_0_4)
+SETTING_14_0_0_2 = copy.copy(SETTING_13_4_0_4)
+SETTING_14_0_0_2['sbflag1'][1].update({
+        'telegram_disable_af':      (HARDWARE.ESP,   '<L', (0xFA0,1,12), (None, '0 <= $ <= 1',                  ('Telegram',    '"TmState {}".format($+6)')) ),
+                                    })
 # ======================================================================
 SETTINGS = [
-            (0x0E000001,0x1000, SETTING_14_0_0_1),
+            (0x0E000002,0x1000, SETTING_14_0_0_2),
             (0x0D040004,0x1000, SETTING_13_4_0_4),
             (0x0D030005,0x1000, SETTING_13_3_0_5),
             (0x0D030002,0x1000, SETTING_13_3_0_2),
